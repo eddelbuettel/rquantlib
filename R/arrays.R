3e6dbd9c4e4dcaa5d9dfc3d2963e1be3b82b90c8
Dirk Eddelbuettel <edd@debian.org>
2010-08-07 18:42:14 +0000

    factored plotOptionSurface() out of demo()

--- R/arrays.R
+++ R/arrays.R
@@ -107,4 +107,56 @@ EuropeanOptionArrays <- function(type, underlying, strike, dividendYield,
     len1 <- length(par1)
     len2 <- length(par2)
     ml <- lapply(val, function(x) matrix(x, len1, len2, dimnames=list(par1,par2)))
+    return(c(ml, parameters=list(type=type, underlying=underlying,
+                 strike=strike, dividendYield=dividendYield,
+                 riskFreeRate=riskFreeRate, maturity=maturity,
+                 volatility=volatility)))
+
+}
+
+plotOptionSurface <- function(EOres, ylabel="", xlabel="", zlabel="", fov=60) {
+    stopifnot(require(rgl))
+    axis.col <- "black"
+    text.col <- axis.col
+    ylab <- ylabel
+    xlab <- xlabel
+    zlab <- zlabel
+    y <- EOres
+
+    ## clear scene:
+    clear3d()
+    clear3d(type="bbox")
+    clear3d(type="lights")
+
+    ## setup env:
+    bg3d(color="#DDDDDD")
+    light3d()
+
+    rgl.viewpoint(fov=fov)
+
+    x <- 1:nrow(y)
+    z <- 1:ncol(y)
+    x <- (x-min(x))/(max(x)-min(x))
+    y <- (y-min(y))/(max(y)-min(y))
+    z <- (z-min(z))/(max(z)-min(z))
+    rgl.surface(x, z, y, alpha=0.6, lit=TRUE, color="blue")
+    rgl.lines(c(0,1), c(0,0), c(0,0), col=axis.col)
+    rgl.lines(c(0,0), c(0,1), c(0,0), col=axis.col)
+    rgl.lines(c(0,0),c(0,0), c(0,1), col=axis.col)
+    rgl.texts(1,0,0, xlab, adj=1, col=text.col)
+    rgl.texts(0,1,0, ylab, adj=1, col=text.col)
+    rgl.texts(0,0,1, zlab, adj=1, col=text.col)
+
+    ## add grid (credit's to John Fox scatter3d)
+    xgridind <- round(seq(1, nrow(y), length=25))
+    zgridind <- round(seq(1, ncol(y), length=25))
+    rgl.surface(x[xgridind], z[zgridind], y[xgridind,zgridind],
+                color="darkgray", alpha=0.5, lit=TRUE,
+                front="lines", back="lines")
+
+    ## animate (credit to rgl.viewpoint() example)
+    start <- proc.time()[3]
+    while ((i <- 36*(proc.time()[3]-start)) < 360) {
+        rgl.viewpoint(i,i/8);
+    }
 }

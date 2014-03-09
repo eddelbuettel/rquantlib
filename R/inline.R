d2c8ad9d3093a177cf025542d18f4355fa094b8d
Dirk Eddelbuettel <edd@debian.org>
2014-03-08 19:31:02 -0600

    moving towards release 0.3.12 further changes following Rcpp 0.11.0

--- R/inline.R
+++ R/inline.R
@@ -1,8 +1,6 @@
 ## RQuantLib -- R interface to the QuantLib libraries
 ##
-## Copyright (C) 2002 - 2013 Dirk Eddelbuettel <edd@debian.org>
-##
-## $Id$
+## Copyright (C) 2002 - 2014  Dirk Eddelbuettel <edd@debian.org>
 ##
 ## This file is part of the RQuantLib library for GNU R.
 ## It is made available under the terms of the GNU General Public
@@ -21,18 +19,30 @@
 .pkgglobalenv <- new.env(parent=emptyenv())
 
 .onLoad <- function(libname, pkgname) {
+
+    ## default to NULL
+    qlcflags <- qllibs <- NULL
+    
     if (.Platform$OS.type=="windows") {
-        ## by convention with CRAN, on Windows we expect these two
-        ## variables to be set, and to be pointing to the respective
-        ## helper installations
-        qlroot   <- Sys.getenv("QUANTLIB_ROOT")
-        boostlib <- Sys.getenv("BOOSTLIB")
-        rarch    <- Sys.getenv("R_ARCH")
-        qlcflags <- sprintf("-I%s -I. -I\"%s\"", qlroot, boostlib)
-        qllibs   <- sprintf("-L%s/lib%s -lQuantLib", rarch)
+        if (Sys.getenv("QUANTLIB_ROOT") != "") {
+            ## by convention with CRAN, on Windows we expect these two
+            ## variables to be set, and to be pointing to the respective
+            ## helper installations
+            qlroot   <- Sys.getenv("QUANTLIB_ROOT")
+            boostlib <- Sys.getenv("BOOSTLIB")
+            rarch    <- Sys.getenv("R_ARCH")
+            qlcflags <- sprintf("-I%s -I. -I\"%s\"", qlroot, boostlib)
+            qllibs   <- sprintf("-L%s/lib%s -lQuantLib", qlroot, rarch)
+        }
     } else {
-        qlcflags <- system( "quantlib-config --cflags", intern = TRUE)
-        qllibs   <- system( "quantlib-config --libs",   intern = TRUE)
+        ## on Linux and OS X, see if we have quantlib-config which may well be
+        ## false in the case of prebuild binary packages as eg r-cran-rquantlib
+        ## on Debian / Ubuntu as well as the OS X package from CRAN
+        qc <- system("bash -c 'type -p quantlib-config'", ignore.stderr=TRUE, intern=TRUE)
+        if (is.character(qc) && nchar(qc) > 1) {
+            qlcflags <- system(paste(qc, "--cflags"), intern = TRUE)
+            qllibs   <- system(paste(qc, "--libs"),   intern = TRUE)
+        }
     }
 
     assign("ql_cflags", qlcflags, envir=.pkgglobalenv)
@@ -40,17 +50,18 @@
 }
 
 LdFlags <- function(print = TRUE) {
+    if (is.null(.pkgglobalenv$ql_libs)) stop("Cannot supply LdFlags as none set.")
     if (print) cat(.pkgglobalenv$ql_libs) else .pkgglobalenv$ql_libs
 }
 
 CFlags <- function(print = TRUE) {
+    if (is.null(.pkgglobalenv$ql_cflags)) stop("Cannot supply CFlags as none set.")
     if (print) cat(.pkgglobalenv$ql_cflags) else .pkgglobalenv$ql_cflags
 }
 
 inlineCxxPlugin <- function(...) {
     plugin <- Rcpp.plugin.maker(include.before = "#include <rquantlib.h>",
-                                libs = sprintf("%s $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)",
-                                               LdFlags(FALSE)),
+                                libs = sprintf("%s $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)", LdFlags(FALSE)),
                                 package = "RQuantLib",
                                 Makevars = NULL,
                                 Makevars.win = NULL)

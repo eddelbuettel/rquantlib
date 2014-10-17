01a969ec66e60a5a86884c0c1fc485f0039bbe54
Dirk Eddelbuettel <edd@debian.org>
2014-10-17 10:21:24 -0500

    adjust parens in test (thanks, Michele!)

--- inst/unitTests/runit.calendar.R
+++ inst/unitTests/runit.calendar.R
@@ -4,9 +4,9 @@
 ## cf test-suite/calendars.cpp in the QL sources
 
 test.isHoliday <- function() {
-    checkEquals(isHoliday("UnitedStates", as.Date("2004-05-01")), TRUE), msg="isHoliday.US")
+    checkEquals(isHoliday("UnitedStates", as.Date("2004-05-01")), TRUE, msg="isHoliday.US")
 }
 
 test.isBusinessDay <- function() {
-    checkEquals(isBusinessDay("UnitedStates", as.Date("2004-04-26")), TRUE), msg="isBusinessDay.US")
+    checkEquals(isBusinessDay("UnitedStates", as.Date("2004-04-26")), TRUE, msg="isBusinessDay.US")
 }

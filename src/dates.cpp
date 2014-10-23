8fe808badb7021b0ff47a72af113f7a306789278
Dirk Eddelbuettel <edd@debian.org>
2014-10-23 06:24:03 -0500

    as<>() and wrap for dates now split between declaration and definition headers

--- src/dates.cpp
+++ src/dates.cpp
@@ -21,46 +21,9 @@
 
 #include <rquantlib.h>
 
-// define template specialisations for as and wrap
-namespace Rcpp {
-    static const unsigned int QLtoJan1970Offset = 25569;  	// Offset to R / Unix epoch 
-
-    template <> QuantLib::Date as(SEXP dtsexp) {
-        Rcpp::Date dt(dtsexp);
-        return QuantLib::Date(static_cast<int>(dt.getDate()) + QLtoJan1970Offset);
-    }
-
-    template <> SEXP wrap(const QuantLib::Date &d) {
-        double dt = static_cast<double>(d.serialNumber()); // QL::BigInteger can cast to double
-        return Rcpp::wrap(Rcpp::Date(dt - QLtoJan1970Offset));
-    }
-
-
-    // non-intrusive extension via template specialisation
-    template <> std::vector<QuantLib::Date> as(SEXP dtvecsexp) {
-        Rcpp::DateVector dtvec(dtvecsexp);
-        int n = dtvec.size();
-        std::vector<QuantLib::Date> dates(n);
-        for (int i = 0; i<n; i++){
-            dates[i] = QuantLib::Date(static_cast<int>(dtvec[i].getDate()) + QLtoJan1970Offset);
-        }
-        return dates;
-    }
-
-    // non-intrusive extension via template specialisation
-    template <> SEXP wrap(const std::vector<QuantLib::Date> &dvec) {
-        int n = dvec.size();
-        Rcpp::DateVector dtvec(n);
-        for (int i = 0; i<n; i++) {
-            // QL::BigInteger can cast to double
-            double dt = static_cast<double>(dvec[i].serialNumber()); 
-            dtvec[i] = Rcpp::Date(dt - QLtoJan1970Offset);
-        }
-        return Rcpp::wrap(dtvec);
-    }
-
-}
-
+// the actual implementation is now in this header which is also pulled into
+// compilation units when using 'plugin' mode via Rcpp Attributes
+#include <rquantlib_impl.h>
 
 // [[Rcpp::interfaces(r, cpp)]]
 

ef1dffbc256084792381a59ac6b02f9cff0b9fe5
Dirk Eddelbuettel <edd@debian.org>
2015-09-10 17:37:01 -0500

    updates suggested by R-devel's R CMD check

--- R/arrays.R
+++ R/arrays.R
@@ -111,49 +111,52 @@ EuropeanOptionArrays <- function(type, underlying, strike, dividendYield,
 }
 
 plotOptionSurface <- function(EOres, ylabel="", xlabel="", zlabel="", fov=60) {
-    stopifnot(require(rgl))
-    utils::globalVariables(c("clear3d", "bg3d", "ligh3d", "rgl.viewpoint", "rgl.surface", "tgl.texts"))
-    axis.col <- "black"
-    text.col <- axis.col
-    ylab <- ylabel
-    xlab <- xlabel
-    zlab <- zlabel
-    y <- EOres
+    if (requireNamespace("rgl", quietly=TRUE)) {
+        utils::globalVariables(c("clear3d", "bg3d", "ligh3d", "rgl.viewpoint", "rgl.surface", "tgl.texts"))
+        axis.col <- "black"
+        text.col <- axis.col
+        ylab <- ylabel
+        xlab <- xlabel
+        zlab <- zlabel
+        y <- EOres
 
-    ## clear scene:
-    rgl::clear3d()
-    rgl::clear3d(type="bbox")
-    rgl::clear3d(type="lights")
+        ## clear scene:
+        rgl::clear3d()
+        rgl::clear3d(type="bbox")
+        rgl::clear3d(type="lights")
 
-    ## setup env:
-    rgl::bg3d(color="#DDDDDD")
-    rgl::light3d()
+        ## setup env:
+        rgl::bg3d(color="#DDDDDD")
+        rgl::light3d()
 
-    rgl::rgl.viewpoint(fov=fov)
+        rgl::rgl.viewpoint(fov=fov)
 
-    x <- 1:nrow(y)
-    z <- 1:ncol(y)
-    x <- (x-min(x))/(max(x)-min(x))
-    y <- (y-min(y))/(max(y)-min(y))
-    z <- (z-min(z))/(max(z)-min(z))
-    rgl::rgl.surface(x, z, y, alpha=0.6, lit=TRUE, color="blue")
-    rgl::rgl.lines(c(0,1), c(0,0), c(0,0), col=axis.col)
-    rgl::rgl.lines(c(0,0), c(0,1), c(0,0), col=axis.col)
-    rgl::rgl.lines(c(0,0),c(0,0), c(0,1), col=axis.col)
-    rgl::rgl.texts(1,0,0, xlab, adj=1, col=text.col)
-    rgl::rgl.texts(0,1,0, ylab, adj=1, col=text.col)
-    rgl::rgl.texts(0,0,1, zlab, adj=1, col=text.col)
+        x <- 1:nrow(y)
+        z <- 1:ncol(y)
+        x <- (x-min(x))/(max(x)-min(x))
+        y <- (y-min(y))/(max(y)-min(y))
+        z <- (z-min(z))/(max(z)-min(z))
+        rgl::rgl.surface(x, z, y, alpha=0.6, lit=TRUE, color="blue")
+        rgl::rgl.lines(c(0,1), c(0,0), c(0,0), col=axis.col)
+        rgl::rgl.lines(c(0,0), c(0,1), c(0,0), col=axis.col)
+        rgl::rgl.lines(c(0,0),c(0,0), c(0,1), col=axis.col)
+        rgl::rgl.texts(1,0,0, xlab, adj=1, col=text.col)
+        rgl::rgl.texts(0,1,0, ylab, adj=1, col=text.col)
+        rgl::rgl.texts(0,0,1, zlab, adj=1, col=text.col)
 
-    ## add grid (credit's to John Fox scatter3d)
-    xgridind <- round(seq(1, nrow(y), length=25))
-    zgridind <- round(seq(1, ncol(y), length=25))
-    rgl::rgl.surface(x[xgridind], z[zgridind], y[xgridind,zgridind],
-                     color="darkgray", alpha=0.5, lit=TRUE,
-                     front="lines", back="lines")
+        ## add grid (credit's to John Fox scatter3d)
+        xgridind <- round(seq(1, nrow(y), length=25))
+        zgridind <- round(seq(1, ncol(y), length=25))
+        rgl::rgl.surface(x[xgridind], z[zgridind], y[xgridind,zgridind],
+                         color="darkgray", alpha=0.5, lit=TRUE,
+                         front="lines", back="lines")
 
-    ## animate (credit to rgl.viewpoint() example)
-    start <- proc.time()[3]
-    while ((i <- 36*(proc.time()[3]-start)) < 360) {
-        rgl::rgl.viewpoint(i,i/8);
+        ## animate (credit to rgl.viewpoint() example)
+        start <- proc.time()[3]
+        while ((i <- 36*(proc.time()[3]-start)) < 360) {
+            rgl::rgl.viewpoint(i,i/8);
+        }
+    } else {
+        message("Please install the 'rgl' package before using this function.")
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# RQuantLib
+## RQuantLib  [![Build Status](https://travis-ci.org/eddelbuettel/rquantlib.svg)](https://travis-ci.org/eddelbuettel/rquantlib) [![License](http://img.shields.io/badge/license-GPL%20%28%3E=%202%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-2.0.html) [![CRAN](http://www.r-pkg.org/badges/version/RQuantLib)](http://cran.rstudio.com/package=RcppRQuantLib) [![Downloads](http://cranlogs.r-pkg.org/badges/RQuantLib?color=brightgreen)](http://www.r-pkg.org/pkg/RQuantLib)
 
-[![Build Status](https://travis-ci.org/eddelbuettel/rquantlib.png)](https://travis-ci.org/eddelbuettel/rquantlib)
+
+[![Build Status](https://travis-ci.org/eddelbuettel/rquantlib.svg](https://travis-ci.org/eddelbuettel/rquantlib)
+
+### About
 
 The RQuantLib package makes parts of [QuantLib](https://github.com/lballabio/quantlib) visible to the R
 user. Currently a number option pricing functions are included, both vanilla
@@ -13,11 +16,11 @@ quantitative finance. The goal is to provide a standard open source library
 for quantitative analysis, modeling, trading, and risk management of
 financial assets.
 
-## Authors
+### Authors
 
 Dirk Eddelbuettel and Khanh Nguyen (during 2009-2010)
 
-## License
+### License
 
 GPL (>= 2)
 

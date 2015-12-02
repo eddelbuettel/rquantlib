1b0737e8f7bfd896ac9e86685df496590a928443
Dirk Eddelbuettel <edd@debian.org>
2015-12-02 11:39:50 -0600

    detect and warn for outdated QL or missing intra-day config

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,6 @@
 ## RQuantLib -- R interface to the QuantLib libraries
 ##
-## Copyright (C) 2002 - 2010  Dirk Eddelbuettel <edd@debian.org>
-##
-## $Id: zzz.R,v 1.3 2005/10/05 03:03:54 edd Exp $
+## Copyright (C) 2002 - 2015  Dirk Eddelbuettel <edd@debian.org>
 ##
 ## This file is part of the RQuantLib library for GNU R.
 ## It is made available under the terms of the GNU General Public
@@ -24,3 +22,21 @@
 #  #cat("This is a pre-release. The interface might change...\n")
 #  library.dynam("RQuantLib", pkg, lib )
 #}
+
+.onAttach <- function(libname, pkgname) {
+
+    ## if it is not interactive (as eg in testing or cronjobs), do nothing
+    if (!interactive()) return(invisible(NULL))
+
+    ## else let's test the QuantLib version, and then the intraday capability
+    qlver <- getQuantLibVersion()
+    if (compareVersion(qlver, "1.7")) {
+        packageStartupMessage("QuantLib version ", qlver, " detected which is older than 1.7.")
+        packageStartupMessage("Intra-daily options analytics unavailable with that version.")
+    } else if (!getQuantLibCapabilities()[["intradayDate"]]) {
+        packageStartupMessage("Sufficient QuantLib version with insuffucient configuration.")
+        packageStartupMessage("Try configuring your build with the --enable-intraday argument.")
+    }
+}
+        
+        

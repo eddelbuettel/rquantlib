6719a94dca50d8b1ade18eb0db6ea8452b4b5ca0
tleitch <tleitch@uchicago.edu>
2016-03-30 21:56:47 -0400

    intermediate result save bermudans

--- R/bermudan.R
+++ R/bermudan.R
@@ -18,109 +18,122 @@
 ##  You should have received a copy of the GNU General Public License
 ##  along with RQuantLib.  If not, see <http://www.gnu.org/licenses/>.
 
-BermudanSwaption <- function(params, tsQuotes, swaptionMaturities,
+BermudanSwaption <- function(params, ts, swaptionMaturities,
                              swapTenors, volMatrix) {
     UseMethod("BermudanSwaption")
 }
 
-BermudanSwaption.default <- function(params, tsQuotes, swaptionMaturities,
+BermudanSwaption.default <- function(params, ts, swaptionMaturities,
                                      swapTenors, volMatrix) {
+    print(class(ts))
+    # Check that params list names
+        
+        if (!is.list(params) || length(params) == 0) {
+            stop("The params parameter must be a non-empty list", call.=FALSE)
+        }
+        if(is.null(params$startDate)){
+            params$startDate=advance("UnitedStates",params$tradeDate, 1, 3)
+            warning("swaption start date not set, defaulting to 1 year from trade date using US calendar")
+        }
+        if(is.null(params$maturity)){
+            params$maturity=advance("UnitedStates",params$startDate, 5, 3)
+            warning("swaption maturity not set, defaulting to 5 years from startDate using US calendar")
+        }
+#         # Check that the term structure quotes are properly formatted.
+#         if(is)
+#             if (!is.list(ts) || length(ts) == 0) {
+#                 stop("Term structure quotes must be a non-empty list", call.=FALSE)
+#             }
+#         if (length(ts) != length(names(ts))) {
+#             stop("Term structure quotes must include labels", call.=FALSE)
+#         }
+#         if (!is.numeric(unlist(ts))) {
+#             stop("Term structure quotes must have numeric values", call.=FALSE)
+#         }
 
-  # Check that params list names
-  if (!is.list(params) || length(params) == 0) {
-      stop("The params parameter must be a non-empty list", call.=FALSE)
-  }
-  if(is.null(params$startDate)){
-      params$startDate=advance("UnitedStates",params$tradeDate, 1, 3)
-    warning("swaption start date not set, defaulting to 1 year from trade date using US calendar")
-  }
-    if(is.null(params$maturity)){
-        params$maturity=advance("UnitedStates",params$startDate, 5, 3)
-        warning("swaption maturity not set, defaulting to 5 years from startDate using US calendar")
+    
+    # Check for correct matrix/vector types
+    if (!is.matrix(volMatrix)
+        || !is.vector(swaptionMaturities)
+        || !is.vector(swapTenors)) {
+        stop("Swaption vol must be a matrix, maturities/tenors must be vectors",
+             call.=FALSE)
     }
-  # Check that the term structure quotes are properly formatted.
-  if (!is.list(tsQuotes) || length(tsQuotes) == 0) {
-    stop("Term structure quotes must be a non-empty list", call.=FALSE)
-  }
-  if (length(tsQuotes) != length(names(tsQuotes))) {
-    stop("Term structure quotes must include labels", call.=FALSE)
-  }
-  if (!is.numeric(unlist(tsQuotes))) {
-    stop("Term structure quotes must have numeric values", call.=FALSE)
-  }
-
-  # Check for correct matrix/vector types
-  if (!is.matrix(volMatrix)
-      || !is.vector(swaptionMaturities)
-      || !is.vector(swapTenors)) {
-    stop("Swaption vol must be a matrix, maturities/tenors must be vectors",
-         call.=FALSE)
-  }
-
-  # Check that matrix/vectors have compatible dimensions
-  if (prod(dim(volMatrix)) != length(swaptionMaturities)*length(swapTenors)) {
-    stop("Dimensions of swaption vol matrix not compatible with maturity/tenor vectors",
-         call.=FALSE)
-  }
-
-  # Finally ready to make the call...
-  # We could coerce types here and pass as.integer(round(swapTenors)),
-  # temp <- as.double(volMatrix), dim(temp) < dim(a) [and pass temp instead
-  # of volMatrix]. But this is taken care of in the C/C++ code.
-  val <- bermudanSwaptionEngine(params, tsQuotes,
-                                swaptionMaturities,
-                                swapTenors, volMatrix)
-  class(val) <- c(params$method, "BermudanSwaption")
-  val
+    
+    # Check that matrix/vectors have compatible dimensions
+    if (prod(dim(volMatrix)) != length(swaptionMaturities)*length(swapTenors)) {
+        stop("Dimensions of swaption vol matrix not compatible with maturity/tenor vectors",
+             call.=FALSE)
+    }
+    
+    # Finally ready to make the call...
+    # We could coerce types here and pass as.integer(round(swapTenors)),
+    # temp <- as.double(volMatrix), dim(temp) < dim(a) [and pass temp instead
+    # of volMatrix]. But this is taken care of in the C/C++ code.
+    if(class(ts)=="DiscountCurve"){
+        print("here")
+        val <- bermudanWithRebuiltCurveEngine(params, c(ts$table$date), ts$table$zeroRates,
+                                      swaptionMaturities,
+                                      swapTenors, volMatrix)   
+    } else{
+        if (!is.numeric(ts) | length(ts) !=1) {
+            stop("Flat Term structure yield must have single numeric value", call.=FALSE)
+        }
+        val <- bermudanFromYieldEngine(params, ts,
+                                  swaptionMaturities,
+                                  swapTenors, volMatrix)
+    }
+    class(val) <- c(params$method, "BermudanSwaption")
+    val
 }
 
 summary.G2Analytic <- function(object,...) {
-  cat('\n\tSummary of pricing results for Bermudan Swaption\n')
-  cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
-  cat('\nStike is ', format(object$params$strike,digits=6))
-  cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
-  cat('\nModel used is: G2/Jamshidian using analytic formulas')
-  cat('\nCalibrated model parameters are:')
-  cat('\na = ', format(object$a,digits=4))
-  cat('\nb = ', format(object$b,digits=4))
-  cat('\nsigma = ', format(object$sigma,digits=4))
-  cat('\neta = ', format(object$eta,digits=4))
-  cat('\nrho = ', format(object$rho,digits=4))
-  cat('\n\n')
+    cat('\n\tSummary of pricing results for Bermudan Swaption\n')
+    cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
+    cat('\nStike is ', format(object$params$strike,digits=6))
+    cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
+    cat('\nModel used is: G2/Jamshidian using analytic formulas')
+    cat('\nCalibrated model parameters are:')
+    cat('\na = ', format(object$a,digits=4))
+    cat('\nb = ', format(object$b,digits=4))
+    cat('\nsigma = ', format(object$sigma,digits=4))
+    cat('\neta = ', format(object$eta,digits=4))
+    cat('\nrho = ', format(object$rho,digits=4))
+    cat('\n\n')
 }
 
 summary.HWAnalytic <- function(object,...) {
-  cat('\n\tSummary of pricing results for Bermudan Swaption\n')
-  cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
-  cat('\nStike is ', format(object$params$strike,digits=6))
-  cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
-  cat('\nModel used is: Hull-White using analytic formulas')
-  cat('\nCalibrated model parameters are:')
-  cat('\na = ', format(object$a,digits=4))
-  cat('\nsigma = ', format(object$sigma,digits=4))
-  cat('\n\n')
+    cat('\n\tSummary of pricing results for Bermudan Swaption\n')
+    cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
+    cat('\nStike is ', format(object$params$strike,digits=6))
+    cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
+    cat('\nModel used is: Hull-White using analytic formulas')
+    cat('\nCalibrated model parameters are:')
+    cat('\na = ', format(object$a,digits=4))
+    cat('\nsigma = ', format(object$sigma,digits=4))
+    cat('\n\n')
 }
 
 summary.HWTree <- function(object,...) {
-  cat('\n\tSummary of pricing results for Bermudan Swaption\n')
-  cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
-  cat('\nStike is ', format(object$params$strike,digits=6))
-  cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
-  cat('\nModel used is: Hull-White using a tree')
-  cat('\nCalibrated model parameters are:')
-  cat('\na = ', format(object$a,digits=4))
-  cat('\nsigma = ', format(object$sigma,digits=4))
-  cat('\n\n')
+    cat('\n\tSummary of pricing results for Bermudan Swaption\n')
+    cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
+    cat('\nStike is ', format(object$params$strike,digits=6))
+    cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
+    cat('\nModel used is: Hull-White using a tree')
+    cat('\nCalibrated model parameters are:')
+    cat('\na = ', format(object$a,digits=4))
+    cat('\nsigma = ', format(object$sigma,digits=4))
+    cat('\n\n')
 }
 
 summary.BKTree <- function(object,...) {
-  cat('\n\tSummary of pricing results for Bermudan Swaption\n')
-  cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
-  cat('\nStike is ', format(object$params$strike,digits=6))
-  cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
-  cat('\nModel used is: Black-Karasinski using a tree')
-  cat('\nCalibrated model parameters are:')
-  cat('\na = ', format(object$a,digits=4))
-  cat('\nsigma = ', format(object$sigma,digits=4))
-  cat('\n\n')
+    cat('\n\tSummary of pricing results for Bermudan Swaption\n')
+    cat('\nPrice (in bp) of Bermudan swaption is ', object$price)
+    cat('\nStike is ', format(object$params$strike,digits=6))
+    cat(' (ATM strike is ', format(object$ATMStrike,digits=6), ')')
+    cat('\nModel used is: Black-Karasinski using a tree')
+    cat('\nCalibrated model parameters are:')
+    cat('\na = ', format(object$a,digits=4))
+    cat('\nsigma = ', format(object$sigma,digits=4))
+    cat('\n\n')
 }

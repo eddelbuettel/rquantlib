be365b5b244e9e331e58ca6d60561ff2f71f6968
tleitch <tleitch@uchicago.edu>
2016-07-28 13:26:47 -0400

    replace use of acast in reshape2 with "for" loops

--- R/sabr.R
+++ R/sabr.R
@@ -104,7 +104,7 @@ SabrSwaption.default <- function(params,
 }
 
 
-volDF2CubeK <- function(params,tbl,source="CME"){
+volDF2CubeK <- function(params,tbl){
   strikes=levels(tbl$Spread)<-c(-200,-150,-100,-75,-50,-25,0,25,50,75,100,150,200)
   
   matYears=as.numeric(params$maturity-params$tradeDate)/365
@@ -115,10 +115,10 @@ volDF2CubeK <- function(params,tbl,source="CME"){
   tenorLvl=c( "1Y", "2Y",  "5Y",  "10Y","15Y","20Y","30Y")
   tbl$Tenor=factor(tbl$Tenor,level=tenorLvl)
   tenors=c(1,2,5,10,15,20,30)
-  tenorIDX=max(findInterval(matYears-expYears,tenors),1)
-  tenorIDX=min(tenorIDX,length(tenors)-3)
-  expiryIDX=findInterval(expYears,expiries)
-  expiryIDX=min(expiryIDX,length(expiries)-3)
+   tenorIDX=max(findInterval(matYears-expYears,tenors),1)
+   tenorIDX=min(tenorIDX,length(tenors)-3)
+   expiryIDX=findInterval(expYears,expiries)
+   expiryIDX=min(expiryIDX,length(expiries)-3)
 
   strikeIDX=1;expire=expLvl[expiryIDX];tenor=tenorLvl[tenorIDX]
 
@@ -135,17 +135,26 @@ volDF2CubeK <- function(params,tbl,source="CME"){
   tbl=tbl[tbl$Tenor%in%tenorLvl,]
   tbl=tbl[tbl$Spread%in%strikes,]
   tbl=tbl[with(tbl,order(Expiry,Tenor,Spread)),]
-  tbl3=tbl[tbl$Spread==0,]
   # atm vol matrix
-  atmMat=acast(tbl3,Expiry~Tenor,value.var = "LogNormalVol")
+  #atmMat=acast(tbl3,Expiry~Tenor,value.var = "LogNormalVol")
+  atmMat=matrix(data=NA,nrow=length(expLvl),ncol=length(tenorLvl),dimnames=list(expLvl,tenorLvl))
+  for(i in 1:length(expLvl)){
+    for(j in 1:length(tenorLvl)){
+      atmMat[i,j]=tbl[tbl$Expiry==expLvl[i]& tbl$Tenor==tenorLvl[j] & tbl$Spread==0,]$LogNormalVol
+    }
+  }
   smirk=matrix(ncol=length(strikes),nrow=length(expLvl)*length(tenorLvl))
 
-  tmp3=acast(tbl,Expiry~Tenor~Spread,value.var="LogNormalVol")
+  #tmp3=acast(tbl,Expiry~Tenor~Spread,value.var="LogNormalVol")
   k=0
   for(i in 1:length(expLvl)){
     for(j in 1:length(tenorLvl)){
       k=k+1
-      smirk[k,1:length(strikes)]=tmp3[i,j,]-tmp3[i,j,"0"]
+      for(n in 1:length(strikes)){
+        #smirk[k,]=tmp3[i,j,]-tmp3[i,j,"0"]
+        smirk[k,n]=tbl[tbl$Expiry==expLvl[i]& tbl$Tenor==tenorLvl[j] & tbl$Spread==strikes[n],]$LogNormalVol -
+          tbl[tbl$Expiry==expLvl[i]& tbl$Tenor==tenorLvl[j] & tbl$Spread==0,]$LogNormalVol
+      }
     }
   }
   

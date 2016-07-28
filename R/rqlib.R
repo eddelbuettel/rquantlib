c051a59c9dff415ccaf2c3e91c31eb83fd38349f
tleitch <tleitch@uchicago.edu>
2016-07-28 18:27:49 -0400

    data documentation

---
+++ R/rqlib.R
@@ -0,0 +1,8 @@
+#' Vol Cube Example.
+#'
+#' Data for valuing swaption examples including rates and a lognormal vol cube
+#'
+#' @format two data frames: \{vcube}, a data frame with four columns: \code{Expiry}, \code{Tenor},
+#'   \code{LogNormalVol},  and \code{Spread} ; and a second tsQuotes with a series of tenors and rates approppriate 
+#'   for calling DiscountCurve
+"rqlib"

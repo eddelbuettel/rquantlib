1106ee33becec3df313f2969b8b36d052e112c5e
Dirk Eddelbuettel <edd@debian.org>
2016-08-04 17:19:08 -0500

    update to data sets to pass R CMD check cleanly
    one file each
    proper Rd file
    R file containing roxygen renamed
    some sanity changes to rquantlib.Rproj as well (four spaces rule :)

---
+++ R/datasets.R
@@ -0,0 +1,21 @@
+#' Vol Cube Example Data
+#'
+#' Data for valuing swaption examples including rates and a lognormal vol cube
+#'
+#' @name vcube
+#' @docType data
+#' @usage data(vcube)
+#' @format two data frames: \code{vcube}, a data frame with four columns: \code{Expiry}, \code{Tenor},
+#' \code{LogNormalVol},  and \code{Spread} 
+#' @source TBA
+NULL
+
+#' Vol Cube Example Data  
+#'  
+#' Short time series examples
+#'  
+#' @name tsQuotes 
+#' @docType data
+#' @format A series of tenors and rates approppriate for calling DiscountCurve
+#' @source TBA
+NULL

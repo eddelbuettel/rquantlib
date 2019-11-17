42cadec8be8fca2e248bbf70174703f5ea6d7602
Dirk Eddelbuettel <edd@debian.org>
2019-11-17 15:36:53 -0600

    step three: convert calendar test

--- inst/tinytest/test_calendar.R
+++ inst/tinytest/test_calendar.R
@@ -3,10 +3,10 @@
 ##
 ## cf test-suite/calendars.cpp in the QL sources
 
-test.isHoliday <- function() {
-    checkEquals(isHoliday("UnitedStates", as.Date("2004-05-01")), TRUE, msg="isHoliday.US")
-}
+library(RQuantLib)
 
-test.isBusinessDay <- function() {
-    checkEquals(isBusinessDay("UnitedStates", as.Date("2004-04-26")), TRUE, msg="isBusinessDay.US")
-}
+#test.isHoliday <- function() {
+expect_true(isHoliday("UnitedStates", as.Date("2004-05-01")), info="isHoliday.US")
+
+#test.isBusinessDay <- function() {
+expect_true(isBusinessDay("UnitedStates", as.Date("2004-04-26")), info="isBusinessDay.US")

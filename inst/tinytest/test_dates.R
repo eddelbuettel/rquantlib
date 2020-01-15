e535b0f760836988161c9239d7f7ffecf2f48f24
Dirk Eddelbuettel <edd@debian.org>
2020-01-15 09:47:24 -0600

    simpler file header for test script

--- inst/tinytest/test_dates.R
+++ inst/tinytest/test_dates.R
@@ -1,21 +1,3 @@
-#!/usr/bin/r -t
-#
-# Copyright (C) 2014 - 2019  Dirk Eddelbuettel
-#
-# This file is part of RQuantLib.
-#
-# RQuantLib is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# RQuantLib is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with RQuantLib.  If not, see <http://www.gnu.org/licenses/>.
 
 .onWindows <- .Platform$OS.type == "windows"
 

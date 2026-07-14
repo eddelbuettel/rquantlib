399029c9f3a7376333bdcefa1a6de61441d709a9
Dirk Eddelbuettel <edd@debian.org>
2026-07-14 09:05:20 -0500

    Simplify tests/tinytest.R by removing no longer needed code

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,15 +1,7 @@
 
-if (requireNamespace("tinytest", quietly=TRUE) &&
-    utils::packageVersion("tinytest") >= "1.0.0") {
-
-    ## Set a seed to make the test deterministic
-    set.seed(42)
-
-    ## R makes us to this
-    Sys.setenv("R_TESTS"="")
-
+if (requireNamespace("tinytest", quietly=TRUE)) {
     ## there are several more granular ways to test files in a tinytest directory,
     ## see its package vignette; tests can also run once the package is installed
-    ## using the same command `test_package(pkgName)`, or by director or file
+    ## using the same command `test_package(pkgName)`, or by directory or file
     tinytest::test_package("RQuantLib", ncpu=getOption("Ncpus", 1))
 }
